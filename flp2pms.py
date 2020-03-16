e544e68cf98a2e214c3367b87d198d47766b126b
Art Goldhammer <art.goldhammer@gmail.com>
2020-03-16 15:02:21 -0400

    reverted to version 3

--- flp2pms.py
+++ flp2pms.py
@@ -1,3 +1,4 @@
+import sys
 import xml.etree.ElementTree as ET
 
 ns1 = {"flp": "http://www8.garmin.com/xmlschemas/FlightPlan/v1"}
@@ -8,15 +9,22 @@ wp_types = {
     "INT": 11,
     "VOR": 3}
 
-header = 'I\n1100 Version\n'
+header = 'I\n3 version\n1'
+dwnld_dir = '/mnt/c/Users/artgo/Downloads/'
+fms_dir = '/mnt/c/X-Plane 11/Output/FMS plans'
 
 def setup():
-    tree = ET.parse('KJFK.fpl')
+    plan = 'KPDX'
+    fname = dwnld_dir + plan + '.fpl'
+    tree = ET.parse(fname)
     root = tree.getroot()
     route = root.find("flp:route", ns1)
+    route_name = route.find('flp:route-name', ns1)
     rtpts = route.findall("flp:route-point", ns1)
     wpts = root.find("flp:waypoint-table", ns1)
-    return rtpts, wpts
+    route_name = route_name.text.split()
+    route_name = '-'.join(route_name)
+    return route_name, rtpts, wpts
 
 def elt_child_text(elt, which_child):
     child = elt.find(f'flp:{which_child}', ns1)
@@ -38,7 +46,7 @@ def make_wptid2wpt_table(wpts):
      for wpt in wpts}
 
 
-def print_fms_rtpt(rtpt, wpt_lkup):
+def print_fms_rtpt(rtpt, wpt_lkup, f):
     wptid = elt_child_text(rtpt, "waypoint-identifier")
     wpt = wpt_lookup_table[wptid]
     wpt_fms = wpt2fms(wpt)
@@ -46,15 +54,20 @@ def print_fms_rtpt(rtpt, wpt_lkup):
     ident = wpt_fms['id']
     lat = wpt_fms['lat']
     lon = wpt_fms['lon']
-    print(f'{typ} {ident} 0.000000 {lon} {lat}')
+    print(f'{typ} {ident} 0.000000 {lat} {lon}', file=f)
 
-def print_fms(rtpts, wpt_lkup):
-    print(header)
-    print(f"NUMENR {len(rtpts)}")
+def print_fms(rtpts, wpt_lkup, f=sys.stdout):
+    print(header, file=f)
+    print(f"{len(rtpts)-1}", file=f)
     for rtpt in rtpts:
-        print_fms_rtpt(rtpt, wpt_lkup)
+        print_fms_rtpt(rtpt, wpt_lkup, f)
 
 if __name__ == "__main__":
-    rtpts, wpts = setup()
+    route_name, rtpts, wpts = setup()
     wpt_lookup_table = make_wptid2wpt_table(wpts)
+    fname = f'{fms_dir}/{route_name}.fms'
+    print(f'Writing to file {fname}')
+    
     print_fms(rtpts, wpt_lookup_table)
+    with open(fname, "w") as f:
+        print_fms(rtpts, wpt_lookup_table, f)
